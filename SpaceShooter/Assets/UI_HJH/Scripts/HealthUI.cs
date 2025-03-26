3c6ab68ff8139b4d143ff64bec3e0d99078b86a3
BonSeol <hjihyun258@gmail.com>
2025-03-27 01:11:51 +0900

    feat:HeartBar
    체력바 생성

---
+++ SpaceShooter/Assets/UI_HJH/Scripts/HealthUI.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public int maxHealth = 10; // 최대 체력
+    public int currentHealth; // 현재 체력
+    public GameObject heartPrefab; // 하트 프리팹
+    public Transform heartContainer; // 하트 UI가 위치할 부모 오브젝트
+
+    private List<GameObject> hearts = new List<GameObject>();
+
+    void Start()
+    {
+        currentHealth = 5;
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        // 기존 하트 삭제
+        foreach (GameObject heart in hearts)
+        {
+            Destroy(heart);
+        }
+        hearts.Clear();
+
+        Debug.Log(heartPrefab.transform.localScale);
+
+        // 새로운 하트 추가
+        for (int i = 0; i < currentHealth; i++)
+        {
+            GameObject heart = Instantiate(heartPrefab, heartContainer);
+            hearts.Add(heart);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth = Mathf.Max(currentHealth - damage, 0);
+        UpdateHearts();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        UpdateHearts();
+    }
+}
